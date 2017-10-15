d94d3360eba2d117d2b0c2d19021acba0a9b52e6
OF-i <orn16@ru.is>
2017-10-15 19:31:57 +0000

    test newLine and comma

--- src/main/java/is/ru/StringCalculator/Calculator.java
+++ src/main/java/is/ru/StringCalculator/Calculator.java
@@ -3,34 +3,34 @@ package is.ru.StringCalculator;
 public class Calculator{
 
   public static int add(String numbers)
-  {
-    if(numbers.equals(""))
-    {
-      return 0;
-    }
-    else
-    {
-      if(numbers.contains(","))
-      {
-        String[] arguments = numbers.split(",");
-        int sum = sum(arguments);
-        return sum;
-      }
-      return parse(numbers);
-    }
-  }
-  private static int parse(String num)
-  {
-	  return Integer.parseInt(num);
-  }
+ {
+   if(numbers.equals(""))
+   {
+     return 0;
+   }
+   else
+   {
+     if(numbers.contains(",") || numbers.contains("\n"))
+     {
+       String[] arguments = numbers.split(",|\n");
+       int sum = sum(arguments);
+       return sum;
+     }
+     return parse(numbers);
+   }
+ }
+ private static int parse(String num)
+ {
+   return Integer.parseInt(num);
+ }
 
-  private static int sum(String[] arguments)
-  {
-    int sum = 0;
-    for(String i : arguments)
-    {
-      sum += parse(i);
-    }
-    return sum;
-  }
+ private static int sum(String[] arguments)
+ {
+   int sum = 0;
+   for(String i : arguments)
+   {
+     sum += parse(i);
+   }
+   return sum;
+ }
 }
